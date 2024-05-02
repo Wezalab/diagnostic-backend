dab4275a4e677c8e0cdcc10ee341819b7f5bc0cf
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-05-02 17:54:59 +0100

    Add session model

---
+++ models/session_model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const sessionSchema = new mongoose.Schema({
+  idCoach: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  idCoachee: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  status: {
+    type: String,
+    enum: ["ACCEPETED", "REJECTED", "DONE", "CANCEL"],
+    default: "PENDING",
+  }
+});
+
+const Session = mongoose.model("Session", sessionSchema);
+module.exports = Session;
