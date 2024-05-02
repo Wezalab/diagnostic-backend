3687b7ce647b22ed76a71c392101af5ebad2d7b3
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-05-02 18:42:15 +0100

    Add routes

---
+++ routes/action-routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const {
+  findAll,
+  getOne,
+  create,
+  update,
+  remove,
+} = require("../controllers/action_controller");
+
+router.get("/", findAll);
+router.get("/:id", getOne);
+router.post("/", create);
+router.put("/:id", update);
+router.delete("/:id", remove);
+
+module.exports = router;
