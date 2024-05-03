4023d03b0adc625599e9f435fabf1bb049ad1fc7
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-05-04 00:35:59 +0100

    Make the sessionId field optional in Mongoose schema

--- models/goal_model.js
+++ models/goal_model.js
@@ -4,23 +4,24 @@ const chat = mongoose.Schema({
   message: { type: String },
   date: { type: Date, default: Date.now() },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  attachements : {type: [String]},
+  attachements: { type: [String] },
 });
 
 const goalSchema = new mongoose.Schema({
-  name: {type: String},
-  coachMood: {type: String},
+  name: { type: String },
+  coachMood: { type: String },
   status: {
     type: String,
-    enum: ["ACCEPETED", "REJECTED","PENDING"],
+    enum: ["ACCEPETED", "REJECTED", "PENDING"],
     default: "PENDING",
   },
   sessionId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Session",
+    required: false, // Making sessionId optional
   },
-  cover: {type: String},
-  label: {type: [String]},
+  cover: { type: String },
+  label: { type: [String] },
   date_limite: { type: Date },
 
   priority: {
@@ -28,11 +29,11 @@ const goalSchema = new mongoose.Schema({
     enum: ["LOW", "MEDIUM", "HIGH"],
     default: "LOW",
   },
-  description: {type: String},
-  attachements : {type: [String]},
+  description: { type: String },
+  attachements: { type: [String] },
   chat: { type: [chat] },
-  percentage: {type: Number},
-  column : {type: Number,  default: 0, },
+  percentage: { type: Number },
+  column: { type: Number, default: 0 },
 });
 
 const Goal = mongoose.model("Goal", goalSchema);
