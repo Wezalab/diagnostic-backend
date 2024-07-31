7b39add5c8f2d92345d2bc43d43f9aec69aa9f1e
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-08-01 00:05:34 +0200

    Add evaluation model

--- models/evaluation_model.js
+++ models/evaluation_model.js
@@ -1,13 +1,30 @@
 const mongoose = require("mongoose");
 
-const socialSchema = new mongoose.Schema({
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  prod_service:[
-    {
-      
-    }
-  ]
+const facteurSchema = mongoose.Schema({
+  nomFacteur: { type: String },
+  questions: [{
+    question: { type: String },
+    recommandations: { type: [String] },
+  }],
+  evaluations: { type: [evaSchema], default: [] },
 });
 
-const Social = mongoose.model("Social", socialSchema);
-module.exports = Social;
+const evaSchema = mongoose.Schema({
+  coach: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Coach
+  coachee: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Coach
+  date: { type: Date, default: Date.now() },
+  score_by_coach: { type: Number },
+  status_by_coach: {
+    type: String,
+    enum: ["En attente", "Necessite Changement", "Refusé", "Approuvé"],
+  },
+});
+
+const evaluationSchema = new mongoose.Schema({
+  questionHeader: { type: String, required: true, trim: true },
+  // Analyse' Outils ' Exécution' Durabilité' facteur 
+  facteur: { type: [facteurSchema], default: [] },
+});
+
+const Evaluation = mongoose.model("Evaluation", evaluationSchema);
+module.exports = Evaluation;
