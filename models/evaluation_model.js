f7dd26b696439ba2603280d22bb587314f16ebc9
guillainbisimwa <guillainbisimwa@hotmail.fr>
2024-08-03 22:32:19 +0200

    Fix issues

--- models/evaluation_model.js
+++ models/evaluation_model.js
@@ -1,5 +1,16 @@
 const mongoose = require("mongoose");
 
+const evaSchema = mongoose.Schema({
+  coach: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Coach
+  coachee: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Coachee
+  date: { type: Date, default: Date.now },
+  score_by_coach: { type: Number },
+  status_by_coach: {
+    type: String,
+    enum: ["En attente", "Necessite Changement", "Refusé", "Approuvé"],
+  },
+});
+
 const facteurSchema = mongoose.Schema({
   nomFacteur: { type: String },
   questions: [{
@@ -9,20 +20,8 @@ const facteurSchema = mongoose.Schema({
   evaluations: { type: [evaSchema], default: [] },
 });
 
-const evaSchema = mongoose.Schema({
-  coach: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Coach
-  coachee: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // Coach
-  date: { type: Date, default: Date.now() },
-  score_by_coach: { type: Number },
-  status_by_coach: {
-    type: String,
-    enum: ["En attente", "Necessite Changement", "Refusé", "Approuvé"],
-  },
-});
-
 const evaluationSchema = new mongoose.Schema({
   questionHeader: { type: String, required: true, trim: true },
-  // Analyse' Outils ' Exécution' Durabilité' facteur 
   facteur: { type: [facteurSchema], default: [] },
 });
 
