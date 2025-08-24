c032ecde593a25bf9104ebcfaacd0abbb97c9250
guillainbisimwa <guillainbisimwa@hotmail.fr>
2025-08-25 01:53:37 +0300

    Update attachment routes to support file uploads

--- routes/attachement-routes.js
+++ routes/attachement-routes.js
@@ -5,12 +5,37 @@ const {
   create,
   update,
   remove,
+  uploadFile,
+  uploadMultipleFiles,
 } = require("../controllers/attachement_controller");
+const { 
+  attachmentUpload, 
+  processImage, 
+  handleUploadError 
+} = require("../middleware/upload");
 
+// Regular CRUD routes
 router.get("/", findAll);
 router.get("/:id", getOne);
 router.post("/", create);
 router.put("/:id", update);
 router.delete("/:id", remove);
+
+// File upload routes
+router.post(
+  "/upload/single",
+  attachmentUpload.single('file'),
+  processImage,
+  handleUploadError,
+  uploadFile
+);
+
+router.post(
+  "/upload/multiple", 
+  attachmentUpload.array('files', 5),
+  processImage,
+  handleUploadError,
+  uploadMultipleFiles
+);
 
 module.exports = router;
